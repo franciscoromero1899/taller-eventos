f2e024a4995da5607d5ff2bbb45739e1698821ac
franciscoromero1899 <141370883+franciscoromero1899@users.noreply.github.com>
2023-08-23 13:19:16 -0300

    Add files via upload
    alerts completos con div

---
+++ events.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const divElement = document.querySelector(".boton");
+  
+    divElement.addEventListener("click", function() {
+      alert("Hola! Soy el div");
+    });
+  });
